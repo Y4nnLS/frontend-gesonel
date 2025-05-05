aef685129ed51ff45dd56d1ca420a6aa173e05c2
Yann Lucas <101208372+Y4nnLS@users.noreply.github.com>
2025-05-05 19:42:26 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import Aura from '@primeuix/themes/aura';
+import PrimeVue from 'primevue/config';
+import ConfirmationService from 'primevue/confirmationservice';
+import ToastService from 'primevue/toastservice';
 
-createApp(App).mount('#app')
+import '@/assets/styles.scss';
+
+const app = createApp(App);
+
+app.use(router);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            darkModeSelector: '.app-dark'
+        }
+    }
+});
+app.use(ToastService);
+app.use(ConfirmationService);
+
+app.mount('#app');
